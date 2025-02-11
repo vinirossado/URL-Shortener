94bfd0d945f4a7d157ef56f89b302d2273f9ed73
Vinicius Rossado <vinicius.rossado@riverty.com>
2025-02-11 17:56:13 +0200

    Adding Base62 logic

---
+++ UrlShortener.Core/Base62EncodingExtensions.cs
@@ -0,0 +1,20 @@
+namespace UrlShortener.Api.Core.Tests;
+
+public static class Base62EncodingExtensions
+{
+    private const string Alphanumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    public static string EncodeToBase62(this int number)
+    {
+        if (number  == 0) return Alphanumeric[0].ToString();
+
+        var result = new Stack<char>();
+
+        while (number > 0)
+        {
+            result.Push(Alphanumeric[number % 62]);
+            number /= 62;
+        }
+
+        return new string(result.ToArray());
+    }
+}
---
+++ UrlShortener.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace UrlShortener.Core;
+
+public class Class1
+{
+}
