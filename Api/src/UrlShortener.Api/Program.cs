81d4e86fc8b87f09233c921e3108ee45905f46ba
Vinicius Rossado <vinicius.rossado@riverty.com>
2025-02-20 10:43:36 +0200

    Adding new project and fixing solutions

--- Api/src/UrlShortener.Api/Program.cs
+++ Api/src/UrlShortener.Api/Program.cs
@@ -32,10 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/", () => "TokenRanges Service");
 
 app.MapPost("/api/urls",
     async (AddUrlHandler handler,
@@ -57,23 +54,4 @@ app.MapPost("/api/urls",
             result.Value);
     });
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
