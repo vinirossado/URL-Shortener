03f3be43978185f175ffef85701a3afd55b3ee96
Vinicius Rossado <vinicius.rossado@riverty.com>
2025-03-13 14:59:23 +0200

    Adding changes for Go API

--- GoService/main.go
+++ GoService/main.go
@@ -14,14 +14,24 @@ func main() {
 		port = "8080"
 	}
 
+	log.Printf("Starting server on port %s", port)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request on /")
 		fmt.Fprintf(w, "Hello World from Go running on Azure App Service!")
 	})
 
 	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request on /api/hello")
 		fmt.Fprintf(w, "Hello from the API endpoint!")
 	})
 
+	// Print startup message
 	log.Printf("Server starting on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	// Listen on all interfaces on the specified port
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
